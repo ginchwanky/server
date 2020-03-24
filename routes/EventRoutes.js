2e6abe71885cf2960283e84aac442dd7ac274100
GanangPrakoso <ganangmanggolo@gmail.com>
2020-03-24 11:43:10 +0700

    dor

--- routes/EventRoutes.js
+++ routes/EventRoutes.js
@@ -5,7 +5,7 @@ const { authentication, authorization } = require('../middlewares/auth')
 
 router.get('/', EventController.findAllEvent)
 router.get('/:id', EventController.findOne)
-router.get('/history/:userId', authentication, EventController.getEventHistory)
+router.get('/history/:userId', EventController.getEventHistory)
 router.post('/', authentication, EventController.createEvent)
 router.put('/:id', authentication, authorization, EventController.updateEvent)
 router.delete('/:id', authentication, authorization,EventController.deleteEvent)
